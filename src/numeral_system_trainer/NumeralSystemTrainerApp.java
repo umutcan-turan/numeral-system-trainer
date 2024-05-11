c68f3410c3843d85455b66e8baea94167321176b
Umutcan Turan <mail@umutcan.org>
2024-05-11 20:22:24 +0300

    Implement NumberUtil

--- src/numeral_system_trainer/NumeralSystemTrainerApp.java
+++ src/numeral_system_trainer/NumeralSystemTrainerApp.java
@@ -8,26 +8,59 @@ class NumeralSystemTrainerApp {
 	
 	public static void run()
 	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
+		System.out.println(Character.getNumericValue('a'));
+		while (true) {
+			String s = kb.nextLine();
+			System.out.println(NumberUtil.decimalToBase(s, 16));
+		}
 	}
 }
 
-class BinaryUtil {
-	public static boolean isValidBinaryNumber(String str)
+class NumberUtil {
+	public static boolean isValidNumber(String str, int base)
 	{
 		for (int i = 0; i < str.length(); i++)
-			if (!isValidBinaryDigit(str.charAt(i)))
+			if (!isValidDigit(str.charAt(i), base))
 				return false;
 		return true;
 	}
 	
-	public static boolean isValidBinaryDigit(char ch)
+	public static boolean isValidDigit(char ch, int base)
 	{
-		return ch == '1' || ch == '0';
+		return switch (base) {
+			case 0, 10 -> isValidDecimalDigit(ch);
+			case 2     -> isValidBinaryDigit(ch);
+			case 16    -> isValidHexDigit(ch);
+			default    -> isValidDecimalDigit(ch);
+		};
 	}
 	
-	public static long binaryToDecimal(String str)
+	public static boolean isValidHexDigit(char ch)
 	{
-		
+		return 'a' <= ch && ch <= 'f' || 'A' <= ch && ch <= 'F' || isValidDecimalDigit(ch);
+	}
+	
+	public static boolean isValidBinaryDigit(char ch)
+	{
+		return ch == '0' || ch == '1';
+	}
+	
+	public static boolean isValidDecimalDigit(char ch)
+	{
+		return '0' <= ch && ch <= '9';
+	}
+	
+	public static String baseToDecimal(String str, int base)
+	{
+		return Integer.toString(Integer.parseInt(str, base), 10);
+	}
+	
+	public static String decimalToBase(String str, int base)
+	{
+		return Integer.toString(Integer.parseInt(str, 10), base);
 	}
 }
+
+
