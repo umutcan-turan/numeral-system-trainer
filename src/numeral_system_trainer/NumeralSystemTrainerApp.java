7cb902e59a78c938ede2f36545aa349821300bcc
Umutcan Turan <mail@umutcan.org>
2024-05-11 20:27:44 +0300

    Simplify NumberUtil

--- src/numeral_system_trainer/NumeralSystemTrainerApp.java
+++ src/numeral_system_trainer/NumeralSystemTrainerApp.java
@@ -9,11 +9,9 @@ class NumeralSystemTrainerApp {
 	public static void run()
 	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
-		
-		System.out.println(Character.getNumericValue('a'));
+
 		while (true) {
-			String s = kb.nextLine();
-			System.out.println(NumberUtil.decimalToBase(s, 16));
+
 		}
 	}
 }
@@ -22,44 +20,19 @@ class NumberUtil {
 	public static boolean isValidNumber(String str, int base)
 	{
 		for (int i = 0; i < str.length(); i++)
-			if (!isValidDigit(str.charAt(i), base))
+			if (Character.digit(str.charAt(i), base) == -1)
 				return false;
 		return true;
 	}
 	
-	public static boolean isValidDigit(char ch, int base)
-	{
-		return switch (base) {
-			case 0, 10 -> isValidDecimalDigit(ch);
-			case 2     -> isValidBinaryDigit(ch);
-			case 16    -> isValidHexDigit(ch);
-			default    -> isValidDecimalDigit(ch);
-		};
-	}
-	
-	public static boolean isValidHexDigit(char ch)
-	{
-		return 'a' <= ch && ch <= 'f' || 'A' <= ch && ch <= 'F' || isValidDecimalDigit(ch);
-	}
-	
-	public static boolean isValidBinaryDigit(char ch)
-	{
-		return ch == '0' || ch == '1';
-	}
-	
-	public static boolean isValidDecimalDigit(char ch)
-	{
-		return '0' <= ch && ch <= '9';
-	}
-	
 	public static String baseToDecimal(String str, int base)
 	{
-		return Integer.toString(Integer.parseInt(str, base), 10);
+		return Long.toString(Long.parseLong(str, base), 10);
 	}
 	
 	public static String decimalToBase(String str, int base)
 	{
-		return Integer.toString(Integer.parseInt(str, 10), base);
+		return Long.toString(Long.parseLong(str, 10), base);
 	}
 }
 
